865b2e8a3f88a78b1f821ac7437b25b386952d12
AngelaPoland <angelalpoland@gmail.com>
2018-07-19 10:55:18 -0700

    started local notifications - right now only triggers manually

--- components/screens/CreateNotifications.js
+++ components/screens/CreateNotifications.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { Actions } from 'react-native-router-flux';
+import { StyleSheet, Text, View, Button, Alert, Platform } from 'react-native';
+// import { Actions } from 'react-native-router-flux';
+import { Notifications, Permissions } from 'expo';
+
+
+async function getiOSNotificationPermission() {
+  const { status } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  if (status !== 'granted') {
+    await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  }
+}
 
 class CreateNotifications extends Component {
+
+  _handleButtonPress = () => {
+    const localnotification = {
+      title: 'This is your drink water reminder!',
+      body: 'Have you gotten to your goal yet? No matter where you are at, hydration will make you feel better.',
+      android: {
+        sound: true,
+      },
+      ios: {
+        sound: true,
+      },
+    };
+    let sendAfterFiveSeconds = Date.now();
+    sendAfterFiveSeconds += 5000;
+
+    const schedulingOptions = { time: sendAfterFiveSeconds };
+    Notifications.scheduleLocalNotificationAsync(
+      localnotification,
+      schedulingOptions
+    );
+  };
+  
+  listenForNotifications = () => {
+    Notifications.addListener(notification => {
+      if (notification.origin === 'received' && Platform.OS === 'ios') {
+        Alert.alert(notification.title, notification.body);
+      }
+    });
+  };
+
+  UNSAFE_componentWillMount() {
+    getiOSNotificationPermission();
+    this.listenForNotifications();
+  }
+
+
+
   render () {
 
   return (
@@ -17,6 +61,10 @@ class CreateNotifications extends Component {
       >
         Create Notifications Page
       </Text>
+      <Button
+          title="Send a notification in 5 seconds!"
+          onPress={this._handleButtonPress}
+        />
     </View>
   );
 }
